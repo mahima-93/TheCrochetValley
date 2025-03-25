e52d13dbe591ca36c85a83539e0e6b875d65f29e
mahima-93 <mahimadalvi06@gmail.com>
2025-03-25 23:08:35 +0530

    end of section 2

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,16 @@
+using Core.Entities;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public decimal Price { get; set; } 
+    public required string PictureUrl { get; set; }
+    public required string Type { get; set; } 
+    public int QuantityInStock{ get; set; } 
+    
+    //TODO -notifyme or preorder
+
+}
